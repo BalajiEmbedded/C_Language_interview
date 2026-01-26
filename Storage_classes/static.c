a4ad2a00814ab0a0391a1b45f835c99404356de6
BalajiEmbedded <balajimantri0204@gmail.com>
2026-01-26 20:44:00 +0530

    added comments

--- Storage_classes/static.c
+++ Storage_classes/static.c
@@ -1,3 +1,10 @@
+//Retains its value between function calls
+//Has a lifetime of the entire program
+//Is initialized only once
+//static local variables have scope with in the function & retains its value & dont reintilized
+//global static varibales have scope of the file
+//static variables are stored in the data section of memmory
+
 #include<stdio.h>
 
 void display()
@@ -6,9 +13,6 @@ void display()
     i=i+1;
     printf("%d\n",i);
 }
-
-
-
 
 void main()
 {
